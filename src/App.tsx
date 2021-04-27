5d9424e157cd5c86bae64f40c654ac68180fa4be
andrian-kars <andrian.karsanashvili@gmail.com>
2021-04-27 17:07:29 +0300

    :tada: Navigation > Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import s from './App.module.scss'
+import { Header } from './components/Header/Header'
+import { HashRouter } from 'react-router-dom'
 
-function App() {
+export const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <div className={s.app}>
+        <Header />
+      </div>
+    </HashRouter>
+  )
 }
-
-export default App;
