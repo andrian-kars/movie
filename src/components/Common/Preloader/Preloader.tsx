dc7774619322ee44f05a98a01a3096374fe20f1d
Andrian Karsanashvili <andrian.karsanashvili@gmail.com>
2023-10-12 00:03:19 +0300

    initial setup

--- src/components/Common/Preloader/Preloader.tsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react'
-import s from './Preloader.module.scss'
-
-export const Preloader: React.FC = memo(() => {
-    return (
-        <div className={s.lds_roller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    )
-})
